ed8df1f15652d292fd502bb1d47ae672a040249d
Vaibhav Kumar <137930085+Projectitgithub@users.noreply.github.com>
2023-12-15 13:39:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <style>
+        body{
+            background-color: yellow;
+
+        }
+        h1{
+            
+            text-align: center;
+            font-size: 50px;
+        }
+        
+           
+         
+        
+        .class{
+            
+            font-size: 40px;
+
+           
+
+
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        Welcome
+        
+    </h1>
+    <div class="class">
+        <h2>Profile:--</h2>
+        <p>My name is Vaibhav Kumar, I have completed my
+        B.tech CSE From Galgotias University. I am 
+        very Meticulous about my work and profession 
+        Since I prefer to do things on time and very precisely
+        and also I am creating new ideas and emerging and new technologies</p>
+    </div>
+    
+    <footer>
+    <div class="cls2">
+        <a href="https://github.com/Projectitgithub">Github</a>
+        <br>
+        <br>
+        <a href="https://www.linkedin.com/in/vaibhav-kumar-936752226/">Linkedin</a>
+    </div>
+    <br>
+    <br>
+
+    <div class="class1">
+    <a href="logout.php">Logout</a>
+    </div>
+    </footer>
+
+
+  
+</body>
+</html>
